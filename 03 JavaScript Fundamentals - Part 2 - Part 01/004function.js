f4b6187fece11161f896e90f11410fc30d02035e
Tushar Karn <113161622+tushar-karn@users.noreply.github.com>
2024-04-18 21:54:13 +0530

    004 javascript function

---
+++ 03 JavaScript Fundamentals - Part 2 - Part 01/004function.js
@@ -0,0 +1,24 @@
+// Function Declaration 
+
+// it can be called before or after the declaration 
+// console.log(calcAge1(2003));  
+
+function calcAge1(BirthYear){
+    return 2024 -BirthYear;
+}
+
+console.log(calcAge1(2003));
+
+// Function Expression 
+
+// it can be called only after the declaration 
+
+
+const age2 = function (BirthYear){
+    return 2024 -BirthYear;
+}
+
+const ans = age2(2003)
+console.log(ans);
+
+
